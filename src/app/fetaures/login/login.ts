a8adfaa5c7bf8bdb0da5f3ffe4a83ff217810036
SrikanthReddy8 <srikanthreddy0808@gmail.com>
2025-12-23 12:57:29 +0530

    login form group & validation changes

--- src/app/fetaures/login/login.ts
+++ src/app/fetaures/login/login.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { Auth } from '../../core/auth';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  constructor(private auth: Auth) {}
-  login() {
-    this.auth.login();
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder, private auth: Auth) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.auth.login();
+    }
   }
 }
