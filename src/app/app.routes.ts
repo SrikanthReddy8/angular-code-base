f5f2e18a83c85efd6d3cffcfab515dda7cb9ce3b
SrikanthReddy8 <srikanthreddy0808@gmail.com>
2025-12-23 13:11:01 +0530

    dashboard changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './fetaures/home/home';
-import { Login } from './fetaures/login/login';
+import { Home } from './features/home/home';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   {
     path: 'jobs',
-    loadChildren: () => import('./fetaures/jobs/jobs.routes').then((m) => m.JOBS_ROUTES),
+    loadChildren: () => import('./features/jobs/jobs.routes').then((m) => m.JOBS_ROUTES),
   },
 ];
